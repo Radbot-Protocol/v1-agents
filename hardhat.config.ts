1dc129b0c89912ab97282d83171cb59cd67c3e24
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 16:57:17 -0700

    startup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,54 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatContractSizer from "@solidstate/hardhat-contract-sizer";
 import { configVariable } from "hardhat/config";
 
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatContractSizer],
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    strict: true,
+  },
+
+  paths: {
+    sources: ["./contracts"],
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+
   solidity: {
     profiles: {
       default: {
         version: "0.8.28",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+          evmVersion: "istanbul",
+          metadata: {
+            bytecodeHash: "none",
+          },
+        },
       },
       production: {
         version: "0.8.28",
         settings: {
+          viaIR: true,
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
+          },
+          evmVersion: "istanbul",
+          metadata: {
+            bytecodeHash: "none",
           },
         },
       },
@@ -35,6 +68,14 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+    base: {
+      type: "http",
+      chainType: "op",
+      url: process.env.BASE_RPC_URL as string,
+      accounts: process.env.BASE_PRIVATE_KEY
+        ? [process.env.BASE_PRIVATE_KEY]
+        : [],
     },
   },
 };
